8d01ae5ecacd09d6444d03b947b296b51bbae77e
rha6780 <rha4578@gmail.com>
2022-03-23 23:33:24 +0900

    모델 필요 테이블 초기 구성

--- Bus_backend/api/v1/models.py
+++ Bus_backend/api/v1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=20_000)
+
+    class Meta:
+        db_table = 'station'
+
+
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=10_000)
+    bus_number = models.IntegerField()
+
+    class Meta:
+        db_table = 'bus'
