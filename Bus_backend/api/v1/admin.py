8252a9fb3231ca60ec63872d2d710dcd4aeebc97
rha6780 <rha4578@gmail.com>
2022-05-07 10:34:16 +0900

    admin model 추가

--- Bus_backend/api/v1/admin.py
+++ Bus_backend/api/v1/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BusStation
+from .models import RoadNode
+from .models import BusRoute
+from .models import NodeLink
 
 # Register your models here.
+# class BusAdmin(admin.ModelAdmin):
+#     pass
+
+admin.register(BusStation)
+admin.register(RoadNode)
+admin.register(BusRoute)
+admin.register(NodeLink)
