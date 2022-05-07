a2172b0f9c56e76c2759c682a97902a3506888f6
rha6780 <rha4578@gmail.com>
2022-05-07 11:15:12 +0900

    pre-commit 설정 수정 및 적용

--- Bus_backend/Bus_backend/urls.py
+++ Bus_backend/Bus_backend/urls.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bus/', include('api.v1.urls'))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("bus/", include("api.v1.urls"))]
--- Bus_backend/api/v1/admin.py
+++ Bus_backend/api/v1/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import BusRoute
 from .models import BusStation
 from .models import NodeLink
